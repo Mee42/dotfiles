96c16daa4aa0f4e005adba22ae107f2b472c3fb9
carson <carson42g@gmail.com>
2019-10-04 20:34:04 -0400

    properly added screen.sh

--- bin/screen.sh
+++ bin/screen.sh
@@ -1,1 +1,2 @@
-.fluxbox/screen.sh
+#!/bin/sh
+xrandr --output DP-0 --primary --mode 1920x1200 --pos 1680x0 --rotate normal --output DVI-D-0 --mode 1680x1050 --pos 0x0 --rotate normal --output DVI-D-1 --off --output DP-1 --off --output HDMI-0 --mode 1920x1080 --pos 3624x0 --rotate normal
