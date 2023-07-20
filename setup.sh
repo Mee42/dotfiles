018927477b6e3d65f5c5410ad35dc178f795b551
Carson Graham <carson42g@gmail.com>
2023-07-20 00:52:26 -0400

    :)

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ln -s $PWD/dot.bashrc ~/.bashrc -v
+ln -s $PWD/dot.bash_profile ~/.bash_profile -v
 ln -s $PWD/dot.gitconfig ~/.gitconfig -v
 mkdir -p ~/.config/nvim
 ln -s $PWD/dot.nvimrc ~/.config/nvim/init.vim -v
-
+ln -s $PWD/tmux.conf ~/.tmux.conf -v
